9bd2e590fa20c5f4adf8ea24ac6a1f211de2af3a
Nasir <70495316+abdinaa17@users.noreply.github.com>
2021-09-27 10:28:55 -0500

    Update longestWordInAString.js

--- longestWordInAString.js
+++ longestWordInAString.js
@@ -1,3 +1,10 @@
+/* Find the Longest Word in a StringPassed
+
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number. */
+
+
 function findLongestWordLength(str) {
   const strArr = str.split(' ')
 
