6eb0a73e601816d4bf88b2140ef057055f4148ea
Nasir <70495316+abdinaa17@users.noreply.github.com>
2021-09-27 10:29:55 -0500

    Update titleCaseASentence.js

--- titleCaseASentence.js
+++ titleCaseASentence.js
@@ -1,3 +1,12 @@
+/* Title Case a SentencePassed
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of. */
+
+
+
+
 function titleCase(str) {
   // We turn the str into an arr to use the map method.
   const strArr = str.toLowerCase().split(' ')
